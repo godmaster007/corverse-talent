eb854f698e639504df1a2f6cbd83200e987ddd80
Nick Dangerous <nicholaskoron+github@gmail.com>
2026-05-11 23:30:19 -0700

    Fix BrowserRouter basename for root domain deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/corverse-talent/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
